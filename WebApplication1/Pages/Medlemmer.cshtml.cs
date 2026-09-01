a29be54394a1c67cd810aa624a50740cbab92cd2
EliasNorbakk <eliasnn@uia.no>
2026-09-01 16:35:50 +0200

    Add project files.

---
+++ WebApplication1/Pages/Medlemmer.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class MedlemmerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
